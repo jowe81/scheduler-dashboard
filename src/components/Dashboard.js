0e9d318cd8ad902c9fac04c659a40df86a4dd2e7
Johannes Weber <johannes@drweber.de>
2022-03-25 13:22:13 -0700

    use local storage

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,19 +1,82 @@
 import React, { Component } from "react";
 import Loading from "./Loading";
+import Panel from "./Panel";
+
 import classnames from "classnames";
+
+
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
 
 class Dashboard extends Component {
 
-  state = { loading: true };
+  state = { 
+    loading: false,
+    focused: null,
+  };
+
+  selectPanel(id) {
+    this.setState(prev => ({
+      focused: prev.focused ? null : id
+    }));
+  }
+
+  //Initialize state after mounting with locally stored state if present
+  componentDidMount() {
+    const focused = JSON.parse(localStorage.getItem("focused"));
+
+    if (focused) {
+      this.setState({focused});
+    }
+  }
+
+  //Store new state if it changed during update
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.focused !== this.state.focused) {
+      localStorage.setItem("focused", JSON.stringify(this.state.focused));
+    }
+  }
 
   render() {
-    const dashboardClasses = classnames("dashboard");
+
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
 
     if (this.state.loading) {
       return <Loading />;
     }
 
-    return <main className={dashboardClasses} />;
+    const panels = data
+      .filter(panel => this.state.focused === null || this.state.focused === panel.id)
+      .map(panel => 
+        <Panel 
+          key={panel.id} 
+          {...panel}
+          onSelect={event => this.selectPanel(panel.id)}>
+        </Panel>);
+
+    return <main className={dashboardClasses}>{panels}</main>;
   }
 }
 
