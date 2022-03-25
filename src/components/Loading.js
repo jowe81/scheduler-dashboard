79f176f004199171eaec5e271237486dbdbd6201
Johannes Weber <johannes@drweber.de>
2022-03-25 11:32:26 -0700

    add Loading.js

---
+++ src/components/Loading.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class Loading extends Component {
+
+  state = { loading: true };
+
+  render() {
+    return <section className="Loading">Loading</section>
+  };
+
+}
